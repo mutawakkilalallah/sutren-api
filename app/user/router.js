e0a789daef5f71a3e19d51cfc1848f5b24abf716
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-10-18 16:46:48 +0700

    menambahkan validasi di router

--- app/user/router.js
+++ app/user/router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { create } = require("./controller");
+const validationCreate = require("../../validation/user-validation-create");
 
 // routing daftar user
-router.post("/", create);
+router.post("/", validationCreate, create);
 
 module.exports = router;
