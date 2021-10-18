e536d3b7fbf28825e7f880de1c5ffd412d43a26a
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-10-18 16:48:21 +0700

    menambahkan model user

---
+++ models/user.js
@@ -0,0 +1,41 @@
+const Sequelize = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    "User",
+    {
+      uuid: {
+        type: DataTypes.UUIDV4,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      nama: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        field: "createdAt",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updatedAt",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "user",
+      timestamp: true,
+    }
+  );
+  return User;
+};
