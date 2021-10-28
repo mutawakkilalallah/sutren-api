66e35f002157304261d611b413d5b60fdbe80df6
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-10-28 19:47:54 +0700

    deployheroku

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const multer = require("multer");
