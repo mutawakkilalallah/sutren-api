b54be95a98708770347b1f0fa38192156d13bf52
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-10-29 17:08:51 +0700

    get all user v3

--- app/user/router.js
+++ app/user/router.js
@@ -15,7 +15,7 @@ const authentication = require("../../middlewares/authentication");
 const { sysadmin } = require("../../middlewares/authorization");
 
 // routing semua data user
-router.get("/", authentication, sysadmin, validationCreate, index);
+router.get("/", authentication, sysadmin, index);
 // routing daftar user
 router.post("/register", authentication, sysadmin, validationCreate, create);
 // routing login user
