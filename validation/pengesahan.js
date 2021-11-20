41f6fe8ee6dd10fe5ee322ef3e02ab5c1e30ac7f
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-11-21 00:52:57 +0700

    error validation custom

--- validation/pengesahan.js
+++ validation/pengesahan.js
@@ -3,9 +3,21 @@ const Joi = require("joi");
 module.exports = {
   // create validation
   create: Joi.object({
-    nama: Joi.string().required(),
-    jabatan: Joi.string().required(),
-    niup: Joi.number().required(),
+    nama: Joi.string().required().messages({
+      "string.base": "nama harus berisi huruf",
+      "string.empty": "nama harus tidak boleh kosong",
+      "any.required": "nama harus harus diisi",
+    }),
+    jabatan: Joi.string().required().messages({
+      "string.base": "jabatan harus berisi huruf",
+      "string.empty": "jabatan harus tidak boleh kosong",
+      "any.required": "jabatan harus harus diisi",
+    }),
+    niup: Joi.number().required().messages({
+      "number.base": "niup harus berisi angka",
+      "number.empty": "niup harus tidak boleh kosong",
+      "any.required": "niup harus harus diisi",
+    }),
   }),
 
   // update validation
