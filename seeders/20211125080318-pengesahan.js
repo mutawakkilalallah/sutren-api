edbc1090bf9fb29f09d867bf4e776433f690fa13
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-11-25 18:38:28 +0700

    fix seeder bug

--- seeders/20211125080318-pengesahan.js
+++ seeders/20211125080318-pengesahan.js
@@ -8,47 +8,47 @@ module.exports = {
         {
           jabatan: "Kepala Pesantren",
           nama: "KH. Abd. Hamid Wahid",
-          niup: 31820500002,
+          niup: 20500002,
         },
         {
           jabatan: "Sekretaris Pesantren",
           nama: "H. Faizin Syamwil, M.Pd.",
-          niup: 31820500078,
+          niup: 20500078,
         },
         {
           jabatan: "Bendahara Pesantren",
           nama: "K. Ahmad Zaki",
-          niup: 31820701748,
+          niup: 20701748,
         },
         {
           jabatan: "Kepala Biro Kepesantrenan",
           nama: "KH. Fahmi Ahz",
-          niup: 31820901773,
+          niup: 20901773,
         },
         {
           jabatan: "Kepala Biro Pengembangan",
           nama: "KH. Faiz",
-          niup: 31820101775,
+          niup: 20101775,
         },
         {
           jabatan: "Kepala Biro Umum",
           nama: "Abdurrohman Wafie",
-          niup: 31820501782,
+          niup: 20501782,
         },
         {
           jabatan: "Kepala Bidang Tarbiyah Wat Ta'lim",
           nama: "Misbahul Munir, M.Pd.I.",
-          niup: 59819509784,
+          niup: 19509784,
         },
         {
           jabatan: "Kepala Bidang Pengembangan Pondok dan Masyarakat (PPM)",
           nama: "Rojabi Azharghany",
-          niup: 31820108256,
+          niup: 20108256,
         },
         {
           jabatan: "Kepala Bidang Kesenian dan Olahraga Santri (BKOS)",
           nama: "KH. Muhammad Maimun",
-          niup: 31820101767,
+          niup: 20101767,
         },
       ],
       {}
