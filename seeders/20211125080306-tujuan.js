8311f0296c5ceb47efff763074c268886b0aaf1d
Mutawakkil Alallah <mutawakkilalallah.19@gmail.com>
2021-12-04 22:03:07 +0700

    update seed tujuan

--- seeders/20211125080306-tujuan.js
+++ seeders/20211125080306-tujuan.js
@@ -7,39 +7,51 @@ module.exports = {
       [
         {
           nama: "Kepala Pesantren",
-          kategori: "Staff/Jabatan",
+          type: "Jabatan",
         },
         {
           nama: "Sekretaris Pesantren",
-          kategori: "Staff/Jabatan",
+          type: "Jabatan",
         },
         {
           nama: "Bendahara Pesantren",
-          kategori: "Staff/Jabatan",
-        },
-        {
-          nama: "Biro Kepesantrenan",
-          kategori: "Instansi/Lembaga",
+          type: "Jabatan",
         },
         {
           nama: "Biro Pengembangan",
-          kategori: "Instansi/Lembaga",
+          type: "Lembaga",
         },
         {
-          nama: "Biro Umum",
-          kategori: "Instansi/Lembaga",
+          nama: "Biro Kepesantrenan",
+          type: "Lembaga",
         },
         {
-          nama: "Bidang Tarbiyah Wat Ta'lim",
-          kategori: "Instansi/Lembaga",
+          nama: "Biro Pendidikan",
+          type: "Lembaga",
         },
         {
-          nama: "Bidang Pengembangan Pondok dan Masyarakat (PPM)",
-          kategori: "Instansi/Lembaga",
+          nama: "KH. Abd. Hamid Wahid, M,Ag.",
+          type: "Person",
         },
         {
-          nama: "Bidang Kesenian dan Olahraga Santri (BKOS)",
-          kategori: "Instansi/Lembaga",
+          nama: "H. Faizin Syamwil, M.Pd.",
+          type: "Person",
+        },
+        {
+          nama: "K. Ahmad Zaki",
+          type: "Person",
+        },
+        {
+          nama: "PP. Sidogiri Pasuruan",
+          type: "Eksternal",
+        },
+        {
+          nama: "PP. Salafiyah Sukerejo",
+          type: "Eksternal",
+        },
+        {
+          nama: "PP. Nurul Islam Jember",
+          type: "Eksternal",
         },
       ],
       {}
